cfc9fb6a1072132eae77675cb21e888b2ab49256
Timothy Goltser <timothy@goltser.me>
2021-10-28 22:28:15 -0400

    Ouch pt. 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,11 @@
 import 'tailwindcss/tailwind.css';
 import { AuthProvider } from '../components/AuthProvider';
-import Head from 'next/head';
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@900&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
             <Component {...pageProps} />
         </AuthProvider>
     );
