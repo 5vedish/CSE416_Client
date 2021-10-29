265723a155e8ef08ea03df6247b22b068a7fae65
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2021-10-29 00:30:30 -0700

    Correct Choice Update & Choice Edit Sizes & Hovers

--- components/quiz/Quiz.tsx
+++ components/quiz/Quiz.tsx
@@ -47,6 +47,27 @@ export default function Quiz({
         await refetch();
     };
 
+    const [isEditing, setEditing] = useState(false);
+
+    const handleEdit = async (e: React.FormEvent<HTMLInputElement>) => {
+        if (
+            parseInt(e.currentTarget.value) < -1 ||
+            parseInt(e.currentTarget.value) > 3
+        ) {
+            alert('Error: Correct choice index out of bounds!');
+            return;
+        }
+
+        console.log(id);
+        await httpClient.put(`/questions/${id}`, {
+            question,
+            choices,
+            correctChoice: e.currentTarget.value,
+        });
+        setEditing(false);
+        await refetch();
+    };
+
     return (
         <div className="w-full h-screen bg-gray-100">
             <form
@@ -93,6 +114,14 @@ export default function Quiz({
                         </p>
                     ))}
                 <DeleteQuiz deleteQuiz={deleteQuiz} />
+                <label className="font-bold text-xl">Set Correct Choice</label>
+                <input
+                    className="appearance-none ml-4 border-2 border-gray-200 rounded w-64 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                    onBlur={handleEdit}
+                    defaultValue={correctChoice}
+                    type="text"
+                    autoFocus={true}
+                ></input>
             </form>
         </div>
     );
