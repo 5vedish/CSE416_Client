06ef46fc956b8ef6acc17e962f4c24ccaadeaf93
Robert <robus.i@hotmail.com>
2021-10-28 22:54:32 -0400

    windows zonk

--- components/Wrapper/FormWrapper.tsx
+++ components/Wrapper/FormWrapper.tsx
@@ -1,7 +1,12 @@
 const FormWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            {children}
+            <div className="space-y-10">
+                <div className="text-center font-logo text-6xl text-blue-500">
+                    Qiz
+                </div>
+                <div className="">{children}</div>
+            </div>
         </div>
     );
 };
