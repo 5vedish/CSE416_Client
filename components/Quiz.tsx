104a30efe9efdc68a16974a25d392a9d84a8bf47
Timothy Goltser <timothy@goltser.me>
2021-10-28 21:43:14 -0400

    Auth state for home screen

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -35,39 +35,36 @@ export default function Quiz({
     };
 
     return (
-        <div className="w-full h-screen bg-blue-200">
-            <Navbar />
-            <form className="bg-gray-50 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                {/* <CreateQuiz func={createQuiz}/> */}
-                <Question refetch={refetch} text={question} id={id} />
-                {answers.map((ans, index) => (
-                    <AnswerChoice
-                        key={ans + index}
-                        text={ans}
-                        index={index}
-                        func={func}
-                        selectedIndex={answerChosen}
-                    />
-                ))}
-
-                <Submit
-                    choice={answerChosen}
-                    func={submit}
-                    setChosen={setChosen}
-                    setCorrect={setCorrect}
+        <form className="bg-gray-50 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            {/* <CreateQuiz func={createQuiz}/> */}
+            <Question refetch={refetch} text={question} id={id} />
+            {answers.map((ans, index) => (
+                <AnswerChoice
+                    key={ans + index}
+                    text={ans}
+                    index={index}
+                    func={func}
+                    selectedIndex={answerChosen}
                 />
-                {correct !== null &&
-                    (correct ? (
-                        <p className="font-bold text-3xl text-green-400">
-                            Correct!
-                        </p>
-                    ) : (
-                        <p className="font-bold text-3xl text-red-400">
-                            Incorrect. Try Again?
-                        </p>
-                    ))}
-                <DeleteQuiz deleteQuiz={deleteQuiz} />
-            </form>
-        </div>
+            ))}
+
+            <Submit
+                choice={answerChosen}
+                func={submit}
+                setChosen={setChosen}
+                setCorrect={setCorrect}
+            />
+            {correct !== null &&
+                (correct ? (
+                    <p className="font-bold text-3xl text-green-400">
+                        Correct!
+                    </p>
+                ) : (
+                    <p className="font-bold text-3xl text-red-400">
+                        Incorrect. Try Again?
+                    </p>
+                ))}
+            <DeleteQuiz deleteQuiz={deleteQuiz} />
+        </form>
     );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                     rel="stylesheet"
                 />
             </Head>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </AuthProvider>
     );
 }
