455889c7334f03c719be86652624c4eac22109a0
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2021-11-04 22:16:10 -0700

    Own Profile Backend Integration

--- pages/own_profile.tsx
+++ pages/own_profile.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from 'next';
 import Navbar from '../components/Navbar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { CurrencyDollarIcon } from '@heroicons/react/solid';
+import { httpClient } from '../lib/axios';
 
 const OwnProfile: NextPage = () => {
     const userInfo = {
@@ -11,35 +12,58 @@ const OwnProfile: NextPage = () => {
         experience: 55000,
     };
 
+    const [displayName, setDisplayName] = useState('');
+    const [currency, setCurrency] = useState(0);
+    const [level, setLevel] = useState(0);
+    const [experience, setExperience] = useState(0);
+
     useEffect(() => {
-        //use dummy data above
-        //fetch data here
+        (async function fetchUser() {
+            await httpClient
+                .get('/me')
+                .then((result) => {
+                    if (result.data) {
+                        const { displayName, currency, level, experience } =
+                            result.data;
+                        setDisplayName(displayName);
+                        setCurrency(currency);
+                        setLevel(level);
+                        setExperience(experience);
+                    }
+                })
+                .catch((e) => {
+                    return;
+                });
+        })();
     }, []);
     return (
         <div className="h-screen overflow-hidden bg-gray-100">
             <Navbar />
             <div className="flex flex-row flex-inline w-full justify-center mt-16 font-logo lg:text-4xl items-center">
-                <p>{userInfo.displayName}</p>
+                <p>{displayName}</p>
                 <CurrencyDollarIcon className="ml-4 w-10 h-10" />
-                <p className="ml-1 text-lg">{userInfo.currency}</p>
+                <p className="ml-1 text-lg">{currency}</p>
             </div>
 
             <div className="flex flex-inline mt-2 text-center justify-center">
-                Lvl.<p className="font-bold">{userInfo.level}</p>
+                Lvl.<p className="font-bold">{level}</p>
             </div>
 
             <div className="relative pt-1 w-1/4 m-auto mt-2">
                 <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
                     <div
-                        style={{ width: '10%' }}
+                        style={{
+                            width: `${
+                                (experience / ((level + 1) * 10000)) * 100
+                            }%`,
+                        }}
                         className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
                     ></div>
                 </div>
                 <div className="flex mb-2 items-center justify-center">
                     <div>
                         <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full bg-gray-200">
-                            {userInfo.experience}/
-                            {(userInfo.level + 1) * 100000}
+                            {experience}/{(level + 1) * 10000}
                         </span>
                     </div>
                 </div>
